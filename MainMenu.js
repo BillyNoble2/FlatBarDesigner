94f5dc925d892c90970bfa905474c9e2c0801500
Billy Noble <billy.noble.2022@uni.strath.ac.uk>
2024-08-10 18:50:47 +0100

    Added UI for gable column design.

--- MainMenu.js
+++ MainMenu.js
@@ -1,3 +1,7 @@
 document.getElementById('flatBarButton').addEventListener('click', () => {
     window.location.href = 'Designer.html';
 });
+
+document.getElementById('gableColButton').addEventListener('click', () => {
+    window.location.href = 'GableColumnDesigner.html';
+});
